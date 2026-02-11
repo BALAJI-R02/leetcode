2ce7cc132fcb655ea5aef62637228aa5eed2685c
BALAJI R <balajicbe0@gmail.com>
2026-02-11 14:00:30 +0530

    Create 495-TeemoAttacking

---
+++ 495-TeemoAttacking/495-TeemoAttacking.java
@@ -0,0 +1,16 @@
+// Last updated: 2/11/2026, 2:00:30 PM
+class Solution {
+    public int findPoisonedDuration(int[] timeSeries, int duration) {
+        int total = 0;
+        for (int i = 0; i < timeSeries.length-1; i++) {
+            
+            if (timeSeries[i+1] <= timeSeries[i] + duration-1) {
+                total += timeSeries[i+1] - timeSeries[i];
+            } else { 
+                total += duration;
+            }
+        }
+        total += duration; 
+        return total;
+    }
+}
