88a2791b33bc90e7339c37f920c25fa36a256eb0
BALAJI R <balajicbe0@gmail.com>
2026-02-11 14:00:44 +0530

    Create 387-FirstUniqueCharacterInAString

---
+++ 387-FirstUniqueCharacterInAString/387-FirstUniqueCharacterInAString.java
@@ -0,0 +1,15 @@
+// Last updated: 2/11/2026, 2:00:44 PM
+class Solution {
+    public int firstUniqChar(String s) {
+       int[]freq=new int[26];
+        for(char c:s.toCharArray()){
+            freq[c-'a']++;
+        }
+        int index=0;
+        for(char c:s.toCharArray()){
+            if(freq[c-'a']==1)return index;
+            index++;
+        }
+        return -1;
+    }
+}
