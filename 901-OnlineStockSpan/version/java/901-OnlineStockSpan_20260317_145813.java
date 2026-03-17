b1980c3dbaae49c9b05d1391b71cf68e333cedd3
BALAJI R <balajicbe0@gmail.com>
2026-03-17 14:58:14 +0530

    Create 901-OnlineStockSpan

---
+++ 901-OnlineStockSpan/version/java/901-OnlineStockSpan_20260317_145813.java
@@ -0,0 +1,12 @@
+// Last updated: 3/17/2026, 2:58:13 PM
+1class StockSpanner {
+2    Stack<int[]> st = new Stack<>();
+3    public int next(int price) {
+4        int d=1;
+5        while(!st.isEmpty() && st.peek()[0]<=price){
+6            d+=st.pop()[1];
+7        }
+8        st.push(new int[]{price,d});
+9        return d;
+10    }
+11}
