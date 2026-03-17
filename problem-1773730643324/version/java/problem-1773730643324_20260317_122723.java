8f681c59875a274ecab56c8143a7aba357b5dd72
BALAJI R <balajicbe0@gmail.com>
2026-03-17 12:27:24 +0530

    Create

---
+++ problem-1773730643324/version/java/problem-1773730643324_20260317_122723.java
@@ -0,0 +1,23 @@
+// Last updated: 3/17/2026, 12:27:23 PM
+class Solution {
+    public int[] smallerNumbersThanCurrent(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[n];
+
+        for(int i = 0; i < n; i++){
+            int count = 0;
+
+            for(int j = 0; j < n; j++){
+                if(nums[j] < nums[i]){
+                    count++;
+                }
+            }
+
+            result[i] = count;
+        }
+
+        return result;
+    }
+}
+        
+    
