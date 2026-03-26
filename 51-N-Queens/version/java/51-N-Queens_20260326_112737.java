48f9e39f82331f571795d4600483b4f366bd1a5e
BALAJI R <balajicbe0@gmail.com>
2026-03-26 11:27:38 +0530

    Create 51-N-Queens

---
+++ 51-N-Queens/version/java/51-N-Queens_20260326_112737.java
@@ -0,0 +1,44 @@
+// Last updated: 3/26/2026, 11:27:37 AM
+1class Solution {
+2    public List<List<String>> res = new ArrayList<>();
+3    public List<List<String>> solveNQueens(int n) {
+4        char board[][] = new char[n][n];
+5        for(int i=0;i<n;i++){
+6            Arrays.fill(board[i],'.');
+7        }
+8        backtrack(board,0);
+9        return res;
+10    }
+11    public void backtrack(char board[][],int row){
+12        if(row==board.length){
+13            ArrayList<String> l = new ArrayList<>();
+14            for(int i=0;i<board.length;i++){
+15                l.add(new String(board[i]));
+16            }
+17            res.add(l);
+18            return;
+19        }
+20        for(int col = 0;col<board[row].length;col++){
+21            if(isSafe(board,row,col)){
+22                board[row][col]='Q';
+23                backtrack(board,row+1);
+24                board[row][col] ='.';
+25            }
+26        }
+27    }
+28    public boolean isSafe(char board[][],int row,int col){
+29        for(int i=0;i<row;i++){
+30            if(board[i][col]=='Q'){
+31                return false;
+32            }
+33            for(int j=0;j<board[row].length;j++){
+34                if(board[i][j]=='Q'){
+35                    if(Math.abs(row-i)==Math.abs(col-j)){
+36                        return false;
+37                    }
+38                }
+39            }
+40        }
+41        return true;
+42    }
+43}
