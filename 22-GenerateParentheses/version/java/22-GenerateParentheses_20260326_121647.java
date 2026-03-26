01e393c6c47e1db45770f15cd7e98813003cb0c2
BALAJI R <balajicbe0@gmail.com>
2026-03-26 12:16:49 +0530

    Create 22-GenerateParentheses

---
+++ 22-GenerateParentheses/version/java/22-GenerateParentheses_20260326_121647.java
@@ -0,0 +1,20 @@
+// Last updated: 3/26/2026, 12:16:47 PM
+1class Solution {
+2    public List<String> res=new ArrayList<>();
+3    public List<String> generateParenthesis(int n) {
+4        gen(0,0,"",n);
+5        return res;
+6    }
+7    public void gen(int op,int cl,String s,int n){
+8        if(s.length()==n*2){
+9            res.add(s);
+10            return;
+11        }
+12        if(op<n){
+13            gen(op+1,cl,s+"(",n);
+14        }
+15        if(cl<op){
+16            gen(op,cl+1,s+")",n);
+17        }
+18    }
+19}
