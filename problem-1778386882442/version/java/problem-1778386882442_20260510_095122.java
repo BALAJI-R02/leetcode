0195e08361656ef34a6ff8b0fd1f19ccff887777
BALAJI R <balajicbe0@gmail.com>
2026-05-10 09:51:23 +0530

    Create

---
+++ problem-1778386882442/version/java/problem-1778386882442_20260510_095122.java
@@ -0,0 +1,23 @@
+// Last updated: 5/10/2026, 9:51:22 AM
+1/**
+2 * Definition for singly-linked list.
+3 * public class ListNode {
+4 *     int val;
+5 *     ListNode next;
+6 *     ListNode() {}
+7 *     ListNode(int val) { this.val = val; }
+8 *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+9 * }
+10 */
+11class Solution {
+12    public ListNode removeElements(ListNode head, int val) {
+13        ListNode ans = new ListNode(0, head);
+14        ListNode dummy = ans;
+15        while (dummy != null) {
+16            while (dummy.next != null && dummy.next.val == val) 
+17                dummy.next = dummy.next.next;
+18            dummy = dummy.next;
+19        }
+20        return ans.next;        
+21    }
+22}
