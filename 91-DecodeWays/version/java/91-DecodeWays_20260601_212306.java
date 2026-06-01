e1ea0fe200a112685b3b416ad58ddaf994de956c
BALAJI R <balajicbe0@gmail.com>
2026-06-01 21:23:06 +0530

    Create 91-DecodeWays

---
+++ 91-DecodeWays/version/java/91-DecodeWays_20260601_212306.java
@@ -0,0 +1,24 @@
+// Last updated: 6/1/2026, 9:23:06 PM
+1public class Solution {
+2    public int numDecodings(String s) {
+3        if (s == null || s.length() == 0 || s.charAt(0) == '0') {
+4            return 0;
+5        }
+6        int n = s.length();
+7        int[] dp = new int[n + 1];
+8        dp[0] = 1;
+9        dp[1] = 1;
+10        for (int i = 2; i <= n; ++i) {
+11            int oneDigit = s.charAt(i - 1) - '0';
+12            int twoDigits = Integer.parseInt(s.substring(i - 2, i));
+13            if (oneDigit != 0) {
+14                dp[i] += dp[i - 1];
+15            }
+16            if (10 <= twoDigits && twoDigits <= 26) {
+17                dp[i] += dp[i - 2];
+18            }
+19        }
+20        return dp[n];
+21    }
+22}
+23
