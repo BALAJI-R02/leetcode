a5b1b309d93de060fa0552b7263077faada6c068
BALAJI R <balajicbe0@gmail.com>
2026-07-23 15:19:38 +0530

    Create 961-N-RepeatedElementinSize2NArray

---
+++ 961-N-RepeatedElementinSize2NArray/version/java/961-N-RepeatedElementinSize2NArray_20260723_151936.java
@@ -0,0 +1,19 @@
+// Last updated: 7/23/2026, 3:19:36 PM
+1class Solution {
+2    public int repeatedNTimes(int[] nums) {
+3        int c=0;
+4        int a=0;
+5        for(int i=0;i<nums.length;i++){
+6            c=0;
+7            for(int j=i;j<nums.length;j++){
+8                if(nums[i]==nums[j]){
+9                    c++;
+10                }
+11
+12            }
+13            if(c==nums.length/2)
+14            a=nums[i];
+15        }
+16        return a;
+17    }
+18}
