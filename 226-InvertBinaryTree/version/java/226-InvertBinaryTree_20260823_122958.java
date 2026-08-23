f0915c16e725fe8e8a5030beade9da1c27a3bcb3
BALAJI R <balajicbe0@gmail.com>
2026-08-23 12:29:58 +0530

    Create 226-InvertBinaryTree

---
+++ 226-InvertBinaryTree/version/java/226-InvertBinaryTree_20260823_122958.java
@@ -0,0 +1,28 @@
+// Last updated: 8/23/2026, 12:29:58 PM
+1/**
+2 * Definition for a binary tree node.
+3 * public class TreeNode {
+4 *     int val;
+5 *     TreeNode left;
+6 *     TreeNode right;
+7 *     TreeNode() {}
+8 *     TreeNode(int val) { this.val = val; }
+9 *     TreeNode(int val, TreeNode left, TreeNode right) {
+10 *         this.val = val;
+11 *         this.left = left;
+12 *         this.right = right;
+13 *     }
+14 * }
+15 */
+16class Solution {
+17    public TreeNode invertTree(TreeNode root) {
+18        if(root==null)
+19        return null;
+20        TreeNode temp=root.left;
+21        root.left=root.right;
+22        root.right=temp;
+23        invertTree(root.left);
+24        invertTree(root.right);
+25        return root;
+26    }
+27}
