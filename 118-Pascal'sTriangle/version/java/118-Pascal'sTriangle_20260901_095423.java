c1f692f4aea541b97efc0ab87a918976ead51461
BALAJI R <balajicbe0@gmail.com>
2026-09-01 09:54:24 +0530

    Create 118-Pascal'sTriangle

---
+++ 118-Pascal'sTriangle/version/java/118-Pascal'sTriangle_20260901_095423.java
@@ -0,0 +1,17 @@
+// Last updated: 9/1/2026, 9:54:23 AM
+1class Solution {
+2    public List<List<Integer>> generate(int numRows) {
+3        List<List<Integer>> list=new ArrayList<>();
+4        for(int i=0;i<numRows;i++){
+5            List<Integer> l=new ArrayList<>();
+6            int value=1;
+7        for(int j=0;j<=i;j++){
+8            
+9            l.add(value);
+10            value=value*(i-j)/(j+1);
+11        }
+12        list.add(l);
+13        }
+14        return list;
+15    }
+16}
