a7baac8a1d09113779397a0fbe9001e8c5b0b69b
BALAJI R <balajicbe0@gmail.com>
2026-09-14 19:15:45 +0530

    Create 836-RectangleOverlap

---
+++ 836-RectangleOverlap/version/java/836-RectangleOverlap_20260914_191544.java
@@ -0,0 +1,10 @@
+// Last updated: 9/14/2026, 7:15:44 PM
+1class Solution {
+2    public boolean isRectangleOverlap(int[] rec1, int[] rec2) {
+3        int left = Math.max(rec1[0], rec2[0]);
+4        int right = Math.min(rec1[2], rec2[2]);
+5        int bottom = Math.max(rec1[1], rec2[1]);
+6        int top = Math.min(rec1[3], rec2[3]);
+7        return left < right && bottom < top;
+8    }
+9}
